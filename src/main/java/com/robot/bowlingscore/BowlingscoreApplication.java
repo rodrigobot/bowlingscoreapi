db946d738c7a078804a1220298ac52d6c426f403
Rodrigo Soto <rodrigo.sotobarrios@gmail.com>
2020-05-07 06:43:33 -0600

    Added app entry point

---
+++ src/main/java/com/robot/bowlingscore/BowlingscoreApplication.java
@@ -0,0 +1,12 @@
+package com.robot.bowlingscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingscoreApplication.class, args);
+	}
+}
