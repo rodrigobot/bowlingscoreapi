a776910fa446e28b0c6fafdd8fa6b818b7ec07e0
Rodrigo Soto <rodrigo.sotobarrios@gmail.com>
2020-05-07 06:48:07 -0600

    Added player test and player model definition

---
+++ src/main/java/com/robot/bowlingscore/model/Player.java
@@ -0,0 +1,59 @@
+package com.robot.bowlingscore.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Entity class that holds information about a player
+ *
+ * @author Rodrigo Soto
+ */
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "name")
+public class Player {
+    @Id
+    private String name;
+
+    // Association of a player and bowling frames
+    @OneToMany(mappedBy = "player",cascade = CascadeType.ALL, targetEntity = BowlingFrame.class, fetch = FetchType.LAZY)
+    private List<BowlingFrame> frames = new ArrayList<>(10);
+
+    // Association of a player and multiple games
+    @ManyToOne
+    @JoinColumn(name = "gameId")
+    private Game game;
+
+    public Player() { }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BowlingFrame> getFrames() {
+        return frames;
+    }
+
+    public void setFrames(List<BowlingFrame> frames) { this.frames = frames; }
+
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+}
---
+++ src/test/java/com/robot/bowlingscore/model/PlayerTests.java
@@ -0,0 +1,67 @@
+package com.robot.bowlingscore.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class PlayerTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private Player p;
+
+    @Before
+    public void setUp() {
+        p = new Player("Test");
+    }
+
+    @After
+    public void tearDown() {
+        p = null;
+    }
+
+    @Test
+    public void testCreatePlayer() {
+        Player savedPlayer = entityManager.persistAndFlush(p);
+
+        assertThat(savedPlayer.getName()).isEqualTo("Test");
+    }
+
+    @Test
+    public void testCreatePlayerSetGame() {
+        Player savedPlayer = entityManager.persistAndFlush(p);
+        Game g = new Game();
+        entityManager.persistAndFlush(g);
+
+        p.setGame(g);
+        savedPlayer = entityManager.persistFlushFind(p);
+
+        assertThat(savedPlayer.getGame().getId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void testSetFrames() {
+        List<BowlingFrame> frames = new ArrayList<>();
+        BowlingFrame frame = new BowlingFrame();
+        frames.add(frame);
+        p.setFrames(frames);
+        Player savedPlayer = entityManager.persistAndFlush(p);
+
+        assertThat(savedPlayer.getFrames().size()).isEqualTo(1);
+    }
+}
