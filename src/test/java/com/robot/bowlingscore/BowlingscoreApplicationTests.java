1a11f514fa31de7ceba56afa9e1b26ea05141fa7
Rodrigo Soto <rodrigo.sotobarrios@gmail.com>
2020-05-07 06:39:50 -0600

    Added test to app entry point

---
+++ src/test/java/com/robot/bowlingscore/BowlingscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.bowlingscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
