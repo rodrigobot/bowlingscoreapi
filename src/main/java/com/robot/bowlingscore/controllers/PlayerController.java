a63cf812275e9923034c7c11da7244610bd9b1c0
Rodrigo Soto <rodrigo.sotobarrios@gmail.com>
2020-05-07 06:58:13 -0600

    Added player controller. It contains no implementation but it is here as a TODO

---
+++ src/main/java/com/robot/bowlingscore/controllers/PlayerController.java
@@ -0,0 +1,8 @@
+package com.robot.bowlingscore.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// TODO: Maybe add endpoints to retrieve player information
+@RestController
+public class PlayerController {
+}
